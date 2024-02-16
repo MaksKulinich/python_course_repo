b522b6049a1382fcc38934935577b8d7887d2c59
Maksym Kulinich <makskulinich00530@gmail.com>
2024-02-16 20:24:50 +0200

    addition a method date to seven string

--- python_core/hw_03/task_1.py
+++ python_core/hw_03/task_1.py
@@ -2,9 +2,9 @@ import datetime
 import re
 
 def get_days_from_today(date:str):
-    now_day = datetime.datetime.today()
+    now_day = datetime.datetime.today().date()
     correct_input_date = re.sub("[^0-9]", "-", date)
-    diference_of_date = now_day - datetime.datetime.strptime(correct_input_date, "%Y-%m-%d")
+    diference_of_date = now_day - datetime.datetime.strptime(correct_input_date, "%Y-%m-%d").date()
     return(diference_of_date.days)
 
-print(get_days_from_today("2122.12=15"))
+print(get_days_from_today("2022.12=15"))
