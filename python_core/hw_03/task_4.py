02d3b1ca82229d16e5f03e000511bedef07ca990
Maksym Kulinich <makskulinich00530@gmail.com>
2024-02-16 20:35:52 +0200

    added function without code

---
+++ python_core/hw_03/task_4.py
@@ -0,0 +1,10 @@
+import datetime
+
+def get_upcoming_birthdays(users:list):
+    pass
+    
+users = [
+    {"name": "John Doe", "birthday": "1985.01.23"},
+    {"name": "Jane Smith", "birthday": "1990.01.27"}
+]
+print(get_upcoming_birthdays(users))
