39f359ec941012d8f4c534e5f38907f422fbc2ce
Maksym Kulinich <makskulinich00530@gmail.com>
2024-02-19 16:42:18 +0200

    import only object datetime, not all liabrary

--- python_core/hw_03/task_1.py
+++ python_core/hw_03/task_1.py
@@ -1,10 +1,10 @@
-import datetime
+from datetime import datetime
 import re
 
 def get_days_from_today(date:str):
-    now_day = datetime.datetime.today().date()
+    now_day = datetime.today().date()
     correct_input_date = re.sub("[^0-9]", "-", date)
-    diference_of_date = now_day - datetime.datetime.strptime(correct_input_date, "%Y-%m-%d").date()
+    diference_of_date = now_day - datetime.strptime(correct_input_date, "%Y-%m-%d").date()
     return(diference_of_date.days)
 
 print(get_days_from_today("2022.12=15"))
