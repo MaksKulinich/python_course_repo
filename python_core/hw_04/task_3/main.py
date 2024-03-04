1e6bb519e609ddb50f5d5cc2f7749f5ace06fa56
Maksym Kulinich <makskulinich00530@gmail.com>
2024-03-04 20:38:53 +0200

    finally project

--- python_core/hw_04/task_3/main.py
+++ python_core/hw_04/task_3/main.py
@@ -1,28 +1,24 @@
-from colorama import Back
+from colorama import init, Fore
 from pathlib import Path
 import sys
 
-def log_file():
-    print(f"{Back.BLUE} [FILE]  This a file")
 
-def log_dir():
-    print(f"{Back.YELLOW} [DIRECTORY] This a directory")
+init()
 
+def main(path):
 
-def proccesing_path(path):
-    
     if path.exists():
         for item in path.iterdir():    
             if item.is_dir():
-                new_path = path / f"{item}"
-                log_dir()
-                proccesing_path(new_path)
+                path = path / f"{item}"
+                print(f"{Fore.YELLOW}  [DIRECTORY] {Fore.RESET} {item.name}")
+                main(path)
             elif item.is_file():
-                log_file()
+                print(f"{Fore.CYAN}  [FILE] {Fore.RESET} {item.name}")
     else:
         print("Not such file or directory")
 
 user_path = Path(sys.argv[1])
 
 if __name__ == "__main__":
-    proccesing_path(user_path)
+    main(user_path)
