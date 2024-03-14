39910d0cc2a6c82efaf1d71302e16b4528993741
Maksym Kulinich <makskulinich00530@gmail.com>
2024-03-14 14:12:04 +0200

    modified file

--- python_core/hw_04/consol_bot(task_4)/main.py
+++ python_core/hw_04/consol_bot(task_4)/main.py
@@ -1,15 +1,17 @@
 from proccesing_function.user_command import add_username_phone, change_username_phone, return_all_phone, return_phone_username, delete_user
 
+
 def parse_user(user_input):
     cmd, *args = user_input.split()
     cmd = cmd.lower().strip()
     return cmd, *args
 
+
 def main():
     contacts = {}
     print("Welcome to the consol bot!")
     print("""
-Here is all commands:
+Here are all the commands:
           
 hello --- returns question
 add --- adds the username and the userphone
@@ -22,11 +24,11 @@ delete --- deletes the contact
         user_input = input("Input your command : ")
 
         command, *args = parse_user(user_input)
-        if len(args) == 2:
-            user_name = args[0]
-            user_phone = args[1]
-        elif len(args) == 1:
-            user_name = args[0]
+        # if len(args) == 2:
+        #     user_name = args[0]
+        #     user_phone = args[1]
+        # elif len(args) == 1:
+        #     user_name = args[0]
 
         if command in ["exit", "close"]:
             print("Good bye!")
@@ -34,17 +36,18 @@ delete --- deletes the contact
         elif command == "hello":
             print("How can I help you?")
         elif command == "add":
-            print(add_username_phone(contacts, user_name, user_phone))
+            print(add_username_phone(contacts, args))
         elif command == "change":
-            print(change_username_phone(contacts, user_name, user_phone))
+            print(change_username_phone(contacts, args))
         elif command == "return":
-            print(return_phone_username(contacts, user_name))
+            print(return_phone_username(contacts, args))
         elif command == "all":
             return_all_phone(contacts)
         elif command == "delete":
-            print(delete_user(contacts, user_name))
+            print(delete_user(contacts, args))
         else:
             print("Invalid command")
+
 
 if __name__ == "__main__":
     main()
--- python_core/hw_04/consol_bot(task_4)/proccesing_function/user_command.py
+++ python_core/hw_04/consol_bot(task_4)/proccesing_function/user_command.py
@@ -1,23 +1,45 @@
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Enter correct user name"
+        except IndexError:
+            return "Enter the correct number of arguments"
 
-def add_username_phone(dictionaty, name, phone):
+    return inner
+
+
+@input_error
+def add_username_phone(dictionaty, args):
+    name, phone = args
     dictionaty[name] = phone
     return "Contact added"
 
 
-def change_username_phone(dictionary, name, phone):
+@input_error
+def change_username_phone(dictionary, args):
+    name, phone = args
     dictionary.update({name : phone})
     return "Phone changed"
 
 
-def return_phone_username(dictionary, name):
+@input_error
+def return_phone_username(dictionary, args):
+    name = args[0]
     return dictionary[name]
 
 
+@input_error
 def return_all_phone(dictionary):
     for values, keys in dictionary.items():
         print(f"{values} : {keys}")
 
 
-def delete_user(dictionary, name):
+@input_error
+def delete_user(dictionary, args):
+    name = args[0]
     del dictionary[name]
     return "User deleted"
