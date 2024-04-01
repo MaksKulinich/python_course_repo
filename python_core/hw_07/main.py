d94f52cb6a0a1b8ed6f3896c3996a7d46fbe56cb
Maksym Kulinich <makskulinich00530@gmail.com>
2024-04-01 13:35:21 +0300

    commit

--- python_core/hw_07/main.py
+++ python_core/hw_07/main.py
@@ -1,7 +1,89 @@
-from proccesing_function.user_command import add_username_phone, change_username_phone, return_all_phone, return_phone_username, delete_user
 from collections import UserDict
 from datetime import datetime, timedelta
 import re
+
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Enter correct user name"
+        except IndexError:
+            return "Enter the correct number of arguments"
+
+    return inner
+
+
+@input_error
+def add_username_phone(dictionaty, args):
+    name, phone = args
+    dictionaty[name] = phone
+    return "Contact added"
+
+
+@input_error
+def change_username_phone(dictionary, args):
+    name, phone = args
+    dictionary.update({name : phone})
+    return "Phone changed"
+
+
+@input_error
+def return_phone_username(dictionary, args):
+    name = args[0]
+    return dictionary[name]
+
+
+@input_error
+def return_all_phone(dictionary):
+    for values, keys in dictionary.items():
+        print(f"{values} : {keys}")
+
+
+@input_error
+def delete_user(dictionary, args):
+    name = args[0]
+    del dictionary[name]
+    return "User deleted"
+
+
+@input_error
+def add_birthday(book, args):
+    name, birthday_str = args
+    record = book.find(name)
+    if record:
+        try:
+            birthday = Birthday(birthday_str)
+            record.birthday = birthday
+            print("Birthday added.")
+        except ValueError as e:
+                print(e)
+        else:
+            print("Contact not found.")
+
+
+@input_error
+def show_birthday(book, args):
+    name = args[0]
+    record = book.find(name)
+    if record and record.birthday:
+        return str(record.birthday)
+    else:
+        return "Birthday not found."
+
+
+@input_error
+def birthdays(book, args = None):
+    upcoming_birthdays = book.get_upcoming_birthdays()
+    if upcoming_birthdays:
+        for entry in upcoming_birthdays:
+            print(f"{entry['name']} has a birthday on {entry['birthday_date']}")
+    else:
+        print("No upcoming birthdays.")
+
 
 class Field:
     def __init__(self, value):
@@ -19,16 +101,16 @@ class Phone(Field):
         if len(phone) == 10:
             super().__init__(phone)
         else:
-            return "Phone must has 10 digits"
+            raise ValueError("Phone must have 10 digits")
 
 
 class Birthday:
     def __init__(self, value):
         try:
-            true_data = re.find("d{2}.d{2}.d{4}", value)
-            if true_data:
-                date_value = datetime.strptime(value, "%d.%m.%Y")
-                self.value = date_value
+            # true_data = re.find("d{2}.d{2}.d{4}", value)
+            # if true_data:
+            date_value = datetime.strptime(value, "%d.%m.%Y")
+            self.value = date_value
         except ValueError:
             raise ValueError("Invalid date format. Use DD.MM.YYYY")
 
@@ -64,9 +146,8 @@ class Record:
         if phone_to_find in self.phones:
             return phone_to_find
         
-    def add_birthday(self, value, name):
-        if name in AddressBook.data:
-            pass
+    def add_birthday(self, value):
+        self.birthday = value
         
 
 
@@ -83,37 +164,34 @@ class AddressBook(UserDict):
         del self.data[name]
 
 
-    def get_upcoming_birthdays(users:list) -> list:
+    def get_upcoming_birthdays(self):
         now_date = datetime.today().date()
         congratulations_list = []
-        for user in users:
-            birthday_date = datetime.strptime(user["birthday"],"%Y.%m.%d").date()
+        for record in self.data.values():
 
-            birthday_date_this_year = datetime.strptime(f"{now_date.year}-{birthday_date.month}-{birthday_date.day}","%Y-%m-%d").date()
+            if record.birthday:
+                birthday_date_this_year = record.birthday.value.replace(year = now_date.year)
 
-            if birthday_date_this_year < now_date:
-                birthday_date_this_year += timedelta(days=birthday_date_this_year.day + 1)
+                if birthday_date_this_year < now_date:
+                    birthday_date_this_year += timedelta(days=birthday_date_this_year.day + 1)
 
-            days_to_birthday = (birthday_date_this_year - now_date).days
+                days_to_birthday = (birthday_date_this_year - now_date).days
 
 
-            if 0 <= days_to_birthday <= 7:
-                if birthday_date_this_year.weekday() >= 5:
-                    days_to_birthday += 7 - birthday_date_this_year.weekday()
-                
-                congratulation_date = now_date + timedelta(days=days_to_birthday)
-                final_congratulation_date = datetime.strftime(congratulation_date, "%Y.%m.%d")
+                if 0 <= days_to_birthday <= 7:
+                    if birthday_date_this_year.weekday() >= 5:
+                        days_to_birthday += 7 - birthday_date_this_year.weekday()
+                    
+                    congratulation_date = now_date + timedelta(days=days_to_birthday)
+                    final_congratulation_date = datetime.strftime(congratulation_date, "%Y.%m.%d")
 
-                congratulations_list.append({
-                    "name" : user["name"],
-                    "congratulation_date" : final_congratulation_date
-                })
+                    congratulations_list.append({
+                        "name" : record.name.value,
+                        "congratulation_date" : final_congratulation_date
+                    })
 
         return congratulations_list
         
-
-
-
 
 def parse_user(user_input):
     cmd, *args = user_input.split()
@@ -154,6 +232,12 @@ delete --- delete the contact
             return_all_phone(book)
         elif command == "delete":
             print(delete_user(book, args))
+        elif command == "add-birthday":
+            add_birthday(book, args)
+        elif command == "show-birthday":
+            print(show_birthday(book, args))
+        elif command == "birthday":
+            print(birthdays(book))
         else:
             print("Invalid command")
 
